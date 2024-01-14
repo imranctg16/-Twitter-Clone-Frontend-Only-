8edc20c485ffcce109ecdd81ea361d80e559caf4
Mohammad Imran Hossain <imran.hossain@brainstaion23.com>
2024-01-15 02:56:27 +0600

    TASK 	- Added the tweet section 	- Tweets made dynamic

--- src/App.vue
+++ src/App.vue
@@ -58,7 +58,103 @@
       </div>
     </div>
     <!-- tweet section -->
-    <div class="w-1/2 h-full">
+    <div class="w-1/2 h-full overflow-y-scroll">
+      <div class="px-5 py-3 border-b border-lighter flex items-center justify-between">
+        <h1 class="text-xl font-bold">Home</h1>
+        <i class="far fa-star text-xl text-blue"></i>
+      </div>
+      <!-- textarea section -->
+      <div class="mt-3 pd-5 px-5 py-3 border-b-8 border-lighter flex">
+        <!-- profile picture -->
+        <div class="flex-none">
+          <img src="profile.png" alt="" class="w-12 h-12 rounded-full border border-lighter">
+        </div>
+        <!-- form section -->
+        <form v-on:submit.prevent="addNewTweet" action="" class="w-full px-4 relative">
+          <textarea v-model="tweet.content" placeholder="What's Up ?" class="w-full focus:outline-none"/>
+          <div class="flex items-center">
+            <i class="text-lg text-blue mr-4 far fa-image"></i>
+            <i class="text-lg text-blue mr-4 fas fa-film"></i>
+            <i class="text-lg text-blue mr-4 far fa-chart-bar"></i>
+            <i class="text-lg text-blue mr-4 far fa-smile"></i>
+          </div>
+          <button type="submit" class="absolute bottom-0 right-0 h-10 px-8 text-white font-semibold bg-blue hover:bg-darkblue focus:outline-none rounded-full">
+            Tweet
+          </button>
+        </form>
+      </div>
+      <!-- tweet index section -->  
+        <!-- my tweets -->
+        <div class="flex flex-col-reverse">
+          <div v-for="tweet in tweets" class="w-full p-4 border-b hover:bg-lighter flex">
+            <div class="flex-none mr-4">
+              <img src="profile.png" class="h-12 w-12 rounded-full flex-none"/>
+            </div>
+            <div class="w-full">
+              <div class="flex items-center w-full">
+                <p class="font-semibold"> Steph Dietz </p>
+                <p class="text-sm text-dark ml-2"> @SaaSyEth </p>
+                <p class="text-sm text-dark ml-2"> 1 sec </p>
+                <i class="fas fa-angle-down text-dark ml-auto"></i>
+              </div>
+              <p class="py-2">
+                {{ tweet.content }}
+              </p>
+              <div class="flex items-center justify-between w-full">
+                <div class="flex items-center text-sm text-dark">
+                  <i class="far fa-comment mr-3"></i>
+                  <p> 0 </p>
+                </div>
+                <div class="flex items-center text-sm text-dark">
+                  <i class="fas fa-retweet mr-3"></i>
+                  <p> 0 </p>
+                </div>
+                <div class="flex items-center text-sm text-dark">
+                  <i class="fas fa-heart mr-3"></i>
+                  <p> 1 </p>
+                </div>
+                <div class="flex items-center text-sm text-dark">
+                  <i class="fas fa-share-square mr-3"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- follower tweets  --> 
+        <div v-for="follow in following" class="w-full p-4 border-b hover:bg-lighter flex">
+          <!-- picture -->
+          <div class="flex-none mr-4">
+            <img src="profile.png" alt="" class="h-12 w-12 rounded-full flex-none">
+          </div>
+          <div class="w-full">
+            <div class="flex items-center w-full">
+              <p class="font-semibold ">{{ follow.name }}</p>
+              <p class="text-sm text-dark ml-2">{{ follow.handle }}</p>
+              <p class="text-sm text-dark ml-2">{{ follow.time }}</p>
+              <i class="fas fa-angle-down text-dark ml-auto"></i>
+            </div>
+            <p class="py-2">{{ follow.tweet }}</p>
+            <div class="flex items-center justify-between w-full">
+              <div class="flex items-center text-sm text-dark">
+                <i class="mr-3 far fa-comment"></i>
+                <p class="">{{ follow.comments }}</p>
+              </div>
+              <div class="flex items-center text-sm text-dark">
+                <i class="mr-3 fas fa-retweet"></i>
+                <p class="">{{ follow.retweets }}</p>
+              </div>
+
+              <div class="flex items-center text-sm text-dark">
+                <i class="mr-3 fas fa-heart"></i>
+                <p class="">{{ follow.like }}</p>
+              </div>
+              <div class="flex items-center text-sm text-dark">
+                <i class="mr-3 fas fa-share-square"></i>
+              </div>
+            </div>
+
+          </div>
+        </div>
     </div>
     <!-- trending section -->
     <div class="md:block hidden w-1/3 border-l border-lighter px-6 py-2  overflow-y-scroll relative">
@@ -133,7 +229,27 @@ export default {
         {src: 'monk.jpg', name: 'Adrian Monk', handle: '@detective:)'},
         {src: 'kevin.jpg', name: 'Kevin Hart', handle: '@miniRock'}
       ],
+      following: [
+        {src: 'elon.jpg', name: 'Elon Musk', handle: '@teslaBoy', time: '20 min', tweet: 'Should I just quarantine on mars??', comments: '1,000', retweets: '550', like: '1,000,003'},
+        {src: 'kevin.jpg', name: 'Kevin Hart', handle: '@miniRock', time: '55 min', tweet: 'Should me and the rock do another sub-par movie together????', comments: '2,030', retweets: '50', like: '20,003'},
+        {src: 'elon.jpg', name: 'Elon Musk', handle: '@teslaBoy', time: '1.4 hr', tweet: 'Haha just made a flame thrower. Shld I sell them?', comments: '100,000', retweets: '1,000,002', like: '5,000,003'},
+        {src: 'elon.jpg', name: 'Elon Musk', handle: '@teslaBoy', time: '1.4 hr', tweet: 'Just did something crazyyyyyyy', comments: '100,500', retweets: '1,000,032', like: '5,000,103'}
+      ],
+      tweets:[
+        {content:'It is so nice outside !'}
+      ],
+      tweet: {
+        content:''
+      }
     };
+  },
+  methods: {
+    addNewTweet(){
+      let newTweet = {
+        content:this.tweet.content
+      }
+      this.tweets.push(newTweet);
+    }
   },
 };
 </script>
